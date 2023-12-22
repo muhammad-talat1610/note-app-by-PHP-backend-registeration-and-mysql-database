2d13fe88d3a469f86a646aa35a5693bc162812b4
Muhammad talat <128619762+muhammad-talat1610@users.noreply.github.com>
2023-12-22 06:04:55 +0200

    final update

--- lib/Apis/linkapi.dart
+++ lib/Apis/linkapi.dart
@@ -1,6 +1,6 @@
 //const String linkServerName2 = "https://waelabohamza.com/coursephp" ;
-const String linkServerName = "http://10.0.2.2/noteapp" ;
-//const String linkServerName = "http://192.168.1.6/noteapp" ;
+//const String linkServerName = "http://10.0.2.2/noteapp" ;
+const String linkServerName = "http://192.168.1.6/noteapp" ;
 
 const String linkImageRoot = "http://waelabohamza.com/coursephp/upload" ;
 // Auth 
